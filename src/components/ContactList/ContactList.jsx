92be551e109c8095502030b83bdf7f635a9c86d1
KL-ML <knataly0513@gmail.com>
2025-06-01 10:45:15 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,28 +4,21 @@ import Heading from '../Heading/Heading';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import css from './ContactList.module.css';
 import { useSelector } from 'react-redux';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 
 export default function ContactList() {
-  const contacts = useSelector(state => state.contacts.items);
-  const filter = useSelector(state => state.filters.name);
-
-  const filteredContacts =
-    filter !== '' && contacts.length > 0
-      ? contacts.filter(contact =>
-          contact.name.toLowerCase().includes(filter.toLowerCase())
-        )
-      : contacts;
+  const contacts = useSelector(selectFilteredContacts);
 
   return (
     <>
-      {filteredContacts.length > 0 ? (
+      {contacts.length > 0 ? (
         <Container variant="innerContainer">
           <Heading variant="cardTitle">
             Contacts <span>| Your contact list</span>
           </Heading>
 
           <ul className={css.contactsList}>
-            {filteredContacts.map(contact => (
+            {contacts.map(contact => (
               <Contact
                 key={contact.id}
                 name={contact.name}
@@ -39,10 +32,8 @@ export default function ContactList() {
             ))}
           </ul>
         </Container>
-      ) : filter ? (
-        <ErrorMessage message="Search result is empty" />
       ) : (
-        <ErrorMessage message="Contact list is empty" />
+        <ErrorMessage message="No contacts found for your search query. Please try again." />
       )}
     </>
   );
